20cce93daf81c11acc05ca89b5488a1fcbe035df
Aromatic <symwww@outlook.com>
2025-07-13 13:38:25 +0800

    feat(GlobalBackground): add wave animiantion

--- client/src/components/GlobalBackground.tsx
+++ client/src/components/GlobalBackground.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import './GlobalBackground.css';
 
 const GlobalBackground: React.FC = () => {
-  return (
-    <div className="global-bg">
-      <div className="glow"></div>
-      <div className="stars"></div>
-      <div className="waves"></div>
-    </div>
-  );
+    return (
+        <div className="global-bg">
+            <div className="glow"></div>
+            <div className="stars"></div>
+            <div className="waves"></div>
+            <ul className="particles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
 };
 
 export default GlobalBackground;
