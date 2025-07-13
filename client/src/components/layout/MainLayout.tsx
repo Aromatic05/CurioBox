91a6fa0554d08dfa8c7015f931e3d194dc445d41
Aromatic <symwww@outlook.com>
2025-07-13 13:47:28 +0800

    feat(MainLayout): add ThemeToggle and GlobalBackground components

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import GlobalBackground from '../GlobalBackground';
+import ThemeToggle from '../ThemeToggle';
 
 function MainLayout() {
     return (
         <div className="min-h-screen flex flex-col">
+            <ThemeToggle />
+            <GlobalBackground />
             <Navbar />
             <main className="flex-grow container mx-auto px-6 py-8">
                 <Outlet />
